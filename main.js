4022675f281cd95e2fc794e394f47a0061fda3a2
0DiegoM <98572984+0DiegoM@users.noreply.github.com>
2022-11-22 19:40:11 -0500

    main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+noseX = 0;
+noseY = 0;
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO)
     video.size(550, 500)
@@ -7,12 +14,21 @@ video.position(40, 260);
     canvas.position(780, 260);
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('Pose', gotPoses);
+    poseNet.on('pose', gotPoses);
 }
 
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("NoseX = "+ noseX +" NoseY = "+ noseY)
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = leftWristX - rightWristX;
+        console.log("leftWristX = "+ leftWristX +" rightWristX = "+ rightWristX+ "Difference = "+ difference);
     }
 }
 
@@ -21,5 +37,9 @@ function modelLoaded() {
 }
 
 function draw(){
-background('#a27bcd')
-}
+    background('#a27bcd')
+    document.getElementById("square_side").innerHTML = 'El ancho y el alto del texto seran '+ Math.floor(difference)+ "px";
+    textSize(difference);
+    fill('#FFE787');
+    text('Hola', noseX, noseY);
+    }
